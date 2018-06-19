52e85d39ff7b25b457a5d5f2fc418909b0fc4a70
Ziyu Chen <461255138@qq.com>
2018-06-19 23:12:29 +0800

    update

--- poem.py
+++ poem.py
@@ -14,6 +14,7 @@ from snownlp import SnowNLP
 import re
 from wordcloud import WordCloud, ImageColorGenerator
 from scipy.misc import imread
+import os
 
 """
 global variable
@@ -35,7 +36,8 @@ class Poem:
     def return_poem_set(self):
         '''
         返回诗歌的集合，是一个list，list每个元素是dict，dict有四个键值，分别对应Poem类的四个静态成员
-        Poem.poem_title\ Poem.poet\ Peom.poem_kind\ Poem.poem
+        Poem.poem_title\ Poem.poet\ Peom.poem_kind\ Poem.poem 对应数据类型
+        str            \ str      \ str           \ list，元素是str
         :return: 返回诗歌的集合
         '''
         return self.poem_set
@@ -50,7 +52,8 @@ class Poem:
         stopwords_data = file_stopword.readlines()
         for word in stopwords_data:
             self.stopwords.append(word.strip())
-        # print(stopwords)
+        self.stopwords.extend(['\n', ' ', Poem.poet, Poem.poem_kind, Poem.poem_title, Poem.poem])
+        print(self.stopwords)
 
     def process(self):
         self.poem_set = []
@@ -78,7 +81,7 @@ class Poem:
                 p.clear()
             line = file.readline()
 
-    def generate_word_cloud(self, textfile, backgroud_pic, save_file, userpic_as_backgroud=False):
+    def generate_word_cloud(self, textfile, backgroud_pic, save_file, userpic_as_backgroud=True):
         '''
         :param textfile: 词云生成的依赖文本
         :param backgroud_pic: 词云生成的背景图片
@@ -88,9 +91,20 @@ class Poem:
         '''
         text = open(textfile, 'r', encoding='utf-8').read()
         pic = imread(backgroud_pic)
+        word_frequency = dict()
+        word_list = jieba.cut(text)
+        for word in word_list:
+            if word not in self.stopwords:
+                word_frequency[word] = word_frequency.get(word, 0) + 1
+        print('word_frequency', word_frequency)
+        # wordcloud = WordCloud(mask=pic, font_path=r'C:\Windows\Fonts\FZYTK.TTF'
+        #                       , background_color='white', margin=2, width=1600, height=900, max_words=700,
+        #                       min_font_size=3, max_font_size=40,
+        #                       random_state=42).generate_from_frequencies(word_frequency)
         wordcloud = WordCloud(mask=pic, font_path=r'C:\Windows\Fonts\FZYTK.TTF'
-                              , background_color='white', margin=5, width=1600, height=900, max_words=500,
-                              min_font_size=10, max_font_size=50, random_state=10).generate(text)
+                              , background_color='white', margin=3, width=1600, height=900, max_words=550,
+                              min_font_size=5, max_font_size=45,
+                              random_state=42).generate_from_frequencies(word_frequency)
         image_colors = ImageColorGenerator(pic)
         if userpic_as_backgroud:
             plt.imshow(wordcloud.recolor(color_func=image_colors))
@@ -101,13 +115,42 @@ class Poem:
 
         wordcloud.to_file(save_file)
 
+    def poet_statistics(self):
+        poet_poems = dict()
+        for p in self.poem_set:
+            poet_poems[p[Poem.poet]] = poet_poems.get(p[Poem.poet], 0) + 1
+        return poet_poems
+
+    def poet_kind_statistics(self):
+        poet_kind_poems = dict()
+        for p in self.poem_set:
+            poet_kind_poems[p[Poem.poem_kind]] = poet_kind_poems.get(p[Poem.poem_kind], 0) + 1
+        return poet_kind_poems
+
+    def generate_wordcloud_for_poet(self, poet, backgroud_pic, save_file):
+        poems_by_poet = ''
+        for p in self.poem_set:
+            if p[Poem.poet] == poet:
+                poems_by_poet += (" ".join([word for word in p[Poem.poem]]))
+                poems_by_poet += " "
+        temp = open('temp.txt', 'w', encoding='utf-8')
+        temp.write(poems_by_poet)
+        temp.close()
+        self.generate_word_cloud('temp.txt', backgroud_pic, save_file, userpic_as_backgroud=False)
+        os.remove('temp.txt')
+
+
+
 
 if __name__ == '__main__':
     pass
-    poem = Poem('poetry.txt')
+    poem = Poem('poetry1.txt')
     poem.set_stopwords('stopwords.txt')
     poem.process()
     poem_set = poem.return_poem_set()
     for one_poem in poem_set:
         print(one_poem)
-    poem.generate_word_cloud('poetry1.txt', 'libai.jpg', 'result.jpg', userpic_as_backgroud=True)
+    poem.generate_word_cloud('poetry1.txt', 'jiubei.jpg', 'jiubei_cw.jpg', userpic_as_backgroud=False)
+    print(poem.poet_statistics())
+    print(poem.poet_kind_statistics())
+    poem.generate_wordcloud_for_poet('李白', 'libai_dufu.jpg', 'libai_cw.jpg')
